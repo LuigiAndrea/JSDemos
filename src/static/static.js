0dfa3a7c177c5841dd4c844bbfea3cb7f5adc166
LAD <luigiandrea@gmail.com>
2018-08-05 12:08:12 -0400

    static es6 and before

---
+++ src/static/static.js
@@ -0,0 +1,33 @@
+class Country {
+    constructor(name, population) {
+        this.population = population;
+        this.name = name
+    }
+
+    GetPopulation() {
+        return this.population;
+    }
+
+    static ComparePopulation(country1, country2) {
+        var r = country1.population - country2.population;
+        return (r > 0) ? 1 : (r == 0) ? 0 : -1;
+    }
+}
+
+function CountryOldWay(name, population) {
+    this.name = name;
+    this.population = population;
+
+    this.GetPopulation = function() { return this.population; }
+}
+
+CountryOldWay.ComparePopulation = function (country1, country2) {
+    var r = country1.population - country2.population;
+    return (r > 0) ? 1 : (r == 0) ? 0 : -1;
+}
+
+module.exports = {
+    Countries : {
+        Country, CountryOldWay
+    }
+}
---
+++ src/static/static.spec.js
@@ -0,0 +1,22 @@
+describe("Static", function () {
+
+    let Countries = require('./static').Countries;
+    let [Country,CountryOldWay] = [Countries.Country,Countries.CountryOldWay];
+
+    it("static es6 with class", function () {
+      var country = new Country('Poland', 38000000);
+      var country2 = new Country('Ireland', 6500000);
+      
+      expect(Country.ComparePopulation(country, country2)).toEqual(1);
+      expect(country.GetPopulation()).toEqual(38000000);
+    });
+  
+    it("static before es6", function () {
+      var country = new CountryOldWay('Finland', 5510000);
+      var country2 = new CountryOldWay('Ireland', 6500000);
+
+      expect(CountryOldWay.ComparePopulation(country, country2)).toEqual(-1);
+      expect(country.GetPopulation()).toEqual(5510000);
+    });
+  });
+  
